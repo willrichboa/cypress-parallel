2c600e06e910231a507c93c404fbf1c9318cd9b5
willrichboa <williamrich@boaweb.com>
2023-04-25 10:03:17 -0500

    add error handling to threads

--- cypress_parallel/thread.js
+++ cypress_parallel/thread.js
@@ -8,12 +8,16 @@ const cypress = require('cypress')
  * @returns a map of the duration and results for each test suite file
  */
 async function executeThread(thread, index) {
+  if (thread.list.length < 1) {
+    throw new Error(`thread for index ${index} was empty before mapping`)
+  }
 
   // legacy sleep. maybe can remove. need to test.
   // staggered start (when executed in container with xvfb ends up having a race condition causing intermittent failures)
-  await new Promise((resolve) => setTimeout(resolve, index * 3000))
-
   const specFiles = `${thread.list.map(path => path).join(',')}`;
+  if (specFiles.length === '') {
+    throw new Error(`list of specFiles for thread at index ${index} was empty after mapping`)
+  }
   const runObj = {
     spec: specFiles,
     reporter: settings.reporter,
@@ -30,7 +34,14 @@ async function executeThread(thread, index) {
   } else {
     runObj['config'] = { video: false }
   }
-  return cypress.run(runObj)
+  await new Promise((resolve) => setTimeout(resolve, index * 3000))
+  console.log(`starting thread with index ${index} with specFiles ${specFiles}`)
+  try {
+    await cypress.run(runObj)
+  } catch (e) {
+    console.error(e)
+    console.error(`error in thread with index ${index}`)
+  }
 
 }
 
